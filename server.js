2d890c916d98fe1ea8da37af362ab70a99b38c36
necfol <necfoll@gmail.com>
2017-06-26 22:04:09 +0800

    add arry host

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const request = require('superagent');
-const { PORT = '3838' } = process.env;
+const { PORT = 3838 } = process.env;
 const { HOST } = require('./package.json').remote;
-const app = express();
-app.set('port', PORT);
-app.use('/', function (req, res) {
-    var sreq = request.get(HOST + req.originalUrl).end((err, sres) => {
-        sres.pipe(res);
-      });
-});
-app.listen(app.get('port'), () => {
-    console.log(`启动服务@${app.get('port')}`)
-});
+var app = [];
+HOST.forEach((hostItem, index) => {
+    app[index] = express();
+    app[index].set('port', PORT + index);
+    app[index].use('/', function (req, res) {
+        var sreq = request.get(HOST[index] + req.originalUrl).end((err, sres) => {
+            sres.pipe(res);
+        });
+    });
+    app[index].listen(app[index].get('port'), () => {
+        console.log(`启动服务@${app[index].get('port')}`)
+    });
+})
